1ddea712a8ca3c1cbdda2de3e8eac5a5b0ead003
Balla Tamás Zsolt <h844410@stud.u-szeged.hu>
2021-06-09 18:46:39 +0200

    pdf generated

--- buildpdf.py
+++ buildpdf.py
@@ -6,13 +6,15 @@ from pathlib import Path
 text = '<meta charset="utf-8">\n'
 
 def add_to_pdf(directory):
+    global text
     with os.scandir(directory) as targyak:
         for targy in targyak:
             with os.scandir(targy) as tetelek:
                 for tetel in tetelek:
-                    with open(tetel, 'r', encoding="utf-8") as f:
-                        content = f.read()
-                        text += content
+                    if ".md" in tetel.name:
+                        with open(tetel, 'r', encoding="utf-8") as f:
+                            content = f.read()
+                            text += content
 
 
 add_to_pdf('TORZSTARGYAK-I')
